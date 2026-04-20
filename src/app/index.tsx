3f72f8fcd9dca6904aa84cd22840737010e55aee
Mateus Villela Consorte <108163301+MVConsorte@users.noreply.github.com>
2026-04-20 17:07:28 -0300

    feat: olá mundo.

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,7 +1,10 @@
-import {Text} from "react-native"
+import {Text, View} from "react-native"
 
 export default function Index() {
     return (
-    <Text>Hello, World!</Text>
+    <View style={{flex: 1, justifyContent: "center", alignItems: "center"}}>
+      <Text>Hello, World!</Text>
+      <Text>Bem vindo à primeira visualização do MVConsorte!</Text>
+    </View>
     )
 }
