93f50d93903923f53a439ec2f068461a69cbbe23
Mateus Villela Consorte <108163301+MVConsorte@users.noreply.github.com>
2026-04-21 00:56:48 -0300

    feat: refactor Index component to use FlatList for rendering links

--- src/app/index/index.tsx
+++ src/app/index/index.tsx
@@ -1,5 +1,5 @@
 import { MaterialIcons } from "@expo/vector-icons";
-import { Image, TouchableOpacity, View } from "react-native";
+import { Image, TouchableOpacity, View, FlatList} from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 import { colors } from "@/styles/colors";
@@ -20,9 +20,22 @@ export default function Index() {
       </View>
       <View style={styles.subcontainer}>
         <Categories />
-      </View>
 
-      <Link name="Rocketseat" url="https://app.rocketseat.com.br/" onDetails={() => console.log("clicou!")}/>
+      <FlatList 
+        data={["1","2","3", "4"]}
+        keyExtractor={item => item}
+        renderItem={() => (
+          <Link 
+            name="Rocketseat" 
+            url="https://app.rocketseat.com.br/" 
+            onDetails={() => console.log("clicou!")}
+          />
+        )}
+        style={styles.links}
+        contentContainerStyle={styles.linksContent}
+        showsVerticalScrollIndicator={false}
+      />
+      </View>
     </SafeAreaView>
   );
 }
