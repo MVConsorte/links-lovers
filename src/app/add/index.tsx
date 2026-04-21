d92ffde8c8f6ad7d344cfe91e1ca79a301c2ace4
Mateus Villela Consorte <108163301+MVConsorte@users.noreply.github.com>
2026-04-21 15:34:41 -0300

    feat: start a screen "Add" component with styles for category selection

---
+++ src/app/add/index.tsx
@@ -0,0 +1,22 @@
+import { View, Text, TouchableOpacity} from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { MaterialIcons } from "@expo/vector-icons";
+
+import { styles } from "./styles";
+import { colors } from "@/styles/colors";
+
+export default function Add(){
+    return (
+        < SafeAreaView style={styles.container}>
+            <View style={styles.header}>
+                <TouchableOpacity>
+                    <MaterialIcons name="arrow-back" size={32} color={colors.gray[200]} />
+                </TouchableOpacity>
+
+                <Text style={styles.title}>Novo</Text>
+            </View>
+
+            <Text style={styles.label}>Selecione uma categoria</Text>
+        </SafeAreaView>
+    )
+}
